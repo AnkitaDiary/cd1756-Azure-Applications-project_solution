257dc63ee8d791204b2ca7e8536050eaa9abb648
AnkitaDubey04 <168834958+AnkitaDiary@users.noreply.github.com>
2026-03-11 00:04:42 +0530

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -132,6 +132,10 @@ def _build_auth_url(authority=None, scopes=None, state=None):
 Routes and views for the flask application.
 """
 
+"""
+Routes and views for the flask application.
+"""
+
 from datetime import datetime
 from flask import render_template, flash, redirect, request, session, url_for
 from werkzeug.urls import url_parse
@@ -140,6 +144,7 @@ from FlaskWebProject import app, db
 from FlaskWebProject.forms import LoginForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
 from FlaskWebProject.models import User, Post
+
 import msal
 import uuid
 import logging
@@ -148,26 +153,33 @@ logger = logging.getLogger(__name__)
 
 imageSourceUrl = 'https://' + app.config['BLOB_ACCOUNT'] + '.blob.core.windows.net/' + app.config['BLOB_CONTAINER'] + '/'
 
+
 @app.route('/')
 @app.route('/home')
 @login_required
 def home():
     user = User.query.filter_by(username=current_user.username).first_or_404()
     posts = Post.query.all()
+
     return render_template(
         'index.html',
         title='Home Page',
         posts=posts
     )
 
+
 @app.route('/new_post', methods=['GET', 'POST'])
 @login_required
 def new_post():
+
     form = PostForm(request.form)
+
     if form.validate_on_submit():
         post = Post()
         post.save_changes(form, request.files['image_path'], current_user.id, new=True)
+
         return redirect(url_for('home'))
+
     return render_template(
         'post.html',
         title='Create Post',
@@ -179,11 +191,16 @@ def new_post():
 @app.route('/post/<int:id>', methods=['GET', 'POST'])
 @login_required
 def post(id):
+
     post = Post.query.get(int(id))
+
     form = PostForm(formdata=request.form, obj=post)
+
     if form.validate_on_submit():
         post.save_changes(form, request.files['image_path'], current_user.id)
+
         return redirect(url_for('home'))
+
     return render_template(
         'post.html',
         title='Edit Post',
@@ -191,14 +208,18 @@ def post(id):
         form=form
     )
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
     if current_user.is_authenticated:
         return redirect(url_for('home'))
 
     form = LoginForm()
 
+    # Normal Admin Login
     if form.validate_on_submit():
+
         user = User.query.filter_by(username=form.username.data).first()
 
         if user is None or not user.check_password(form.password.data):
@@ -206,16 +227,18 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
 
-        logger.info("admin logged in successfully")
+        logger.info("Admin logged in successfully")
 
         login_user(user, remember=form.remember_me.data)
 
         next_page = request.args.get('next')
+
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
 
         return redirect(next_page)
 
+    # Microsoft Login
     session["state"] = str(uuid.uuid4())
 
     auth_url = _build_auth_url(
@@ -247,7 +270,7 @@ def authorized():
         result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
             request.args['code'],
             scopes=Config.SCOPE,
-            redirect_uri=url_for('authorized', _external=True)
+            redirect_uri="https://cmswebapp123-g6ababc4dvcnapgb.centralus-01.azurewebsites.net/getAToken"
         )
 
         if "error" in result:
@@ -255,8 +278,12 @@ def authorized():
 
         session["user"] = result.get("id_token_claims")
 
+        # After Microsoft login, log in as admin
         user = User.query.filter_by(username="admin").first()
+
         login_user(user)
+
+        logger.info("Microsoft login successful")
 
         _save_cache(cache)
 
@@ -280,6 +307,10 @@ def logout():
 
     return redirect(url_for('login'))
 
+
+# -------------------------------
+# MSAL Helper Functions
+# -------------------------------
 
 def _load_cache():
 
@@ -312,5 +343,5 @@ def _build_auth_url(authority=None, scopes=None, state=None):
     return _build_msal_app(authority=authority).get_authorization_request_url(
         scopes,
         state=state,
-        redirect_uri=url_for("authorized", _external=True)
+        redirect_uri="https://cmswebapp123-g6ababc4dvcnapgb.centralus-01.azurewebsites.net/getAToken"
     )
